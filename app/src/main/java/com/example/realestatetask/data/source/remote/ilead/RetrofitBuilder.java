baa1029cdc94bffe2f9c529233437f4326f72d31
Hassan <hassansobhy60@gmail.com>
2022-11-13 16:01:38 +0200

    add okhttp client and logging interceptor

--- app/src/main/java/com/example/realestatetask/data/source/remote/ilead/RetrofitBuilder.java
+++ app/src/main/java/com/example/realestatetask/data/source/remote/ilead/RetrofitBuilder.java
@@ -1,5 +1,10 @@
 package com.example.realestatetask.data.source.remote.ilead;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -15,6 +20,7 @@ public class RetrofitBuilder {
             synchronized (RetrofitBuilder.class) {
                 if (retrofit == null) {
                     retrofit = new Retrofit.Builder()
+                            .client(initOkHttp())
                             .addConverterFactory(GsonConverterFactory.create())
                             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                             .baseUrl(URL).build();
@@ -22,5 +28,17 @@ public class RetrofitBuilder {
             }
         }
         return retrofit;
+    }
+
+    private static OkHttpClient initOkHttp() {
+        int REQUEST_TIMEOUT = 90;
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                .readTimeout(REQUEST_TIMEOUT, TimeUnit.SECONDS)
+                .connectTimeout(REQUEST_TIMEOUT, TimeUnit.SECONDS)
+                .writeTimeout(REQUEST_TIMEOUT, TimeUnit.SECONDS);
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.addInterceptor(httpLoggingInterceptor);
+        return builder.retryOnConnectionFailure(true).build();
     }
 }
